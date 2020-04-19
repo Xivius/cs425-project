99c602401d902747424fad9cddc05a57959a372c
IonatanGiurgiu <32996929+IonatanGiurgiu@users.noreply.github.com>
2020-04-19 17:29:13 -0500

    Added the skeleton for the rest of the project.

--- src/client/Application.java
+++ src/client/Application.java
@@ -38,9 +38,9 @@ public class Application {
             String privilege = null;
             while(true){
                 /* Login Prompt */
-                System.out.print("Username: ");
+                System.out.println("Username: ");
                 username = scan.nextLine();
-                System.out.print("Password: ");
+                System.out.println("Password: ");
                 password = scan.nextLine();
                 
                 // Find out the priviledge of the person who logged in.
@@ -63,16 +63,94 @@ public class Application {
                 
                 // If there is no match, keep going in the loop, else break.
                 if (privilege == null){
-                    System.out.println("Username and password does not match");
+                    System.out.println("Username and/or password incorrect");
                 } else {
                     break;
                 }
             }
             
             
-            
-                                    
-            
+            // Create while loops for each privilege (type of user):
+            if (privilege == "admin"){
+                while (true) {
+                    System.out.println("What would you like to do? (Type number) Options:");
+                    System.out.println("(1) Create a new Employee");
+                    System.out.println("(2) Update a table");
+                    System.out.println("(3) Grant access to an Employee");
+                    System.out.println("(4) Business analytics");
+                    System.out.println("(5) Logout");
+                    String userInput = scan.nextLine();
+                    
+                    // For each option, write the code to satisfy it.
+                    if (userInput == "5"){
+                        break;
+                    } else if (userInput == "4"){
+                        // FIXME: Need to write the code for each action
+                    }
+                    
+                    
+                    
+                }
+            } else if (privilege == "sales"){
+                while (true) {
+                    System.out.println("What would you like to do? (Type number) Options:");
+                    System.out.println("(1) View/update a Customer");
+                    System.out.println("(2) Create an Order");
+                    System.out.println("(3) Access sales reports");
+                    System.out.println("(4) Logout");
+                    String userInput = scan.nextLine();
+                    
+                    // For each option, write the code to satisfy it.
+                    if (userInput == "4"){
+                        break;
+                    } else if (userInput == "3"){
+                        // FIXME: Need to write the code for each action
+                    }
+                    
+                    
+                    
+                }
+            } else if (privilege == "hr"){
+                while (true) {
+                    System.out.println("What would you like to do? (Type number) Options:");
+                    System.out.println("(1) View/update an Employee's information");
+                    System.out.println("(2) View sales for an Employee");
+\                   System.out.println("(3) Logout");
+                    String userInput = scan.nextLine();
+                    
+                    // For each option, write the code to satisfy it.
+                    if (userInput == "3"){
+                        break;
+                    } else if (userInput == "2"){
+                        // FIXME: Need to write the code for each action
+                    }
+                    
+                    
+                    
+                }
+            } else if (privilege == "engineering"){
+                while (true) {
+                    System.out.println("What would you like to do? (Type number) Options:");
+                    System.out.println("(1) View/update the Inventory");
+                    System.out.println("(2) View/update a Model");
+\                   System.out.println("(3) View Employee information");
+                    System.out.println("(4) Logout");
+                    String userInput = scan.nextLine();
+                    
+                    // For each option, write the code to satisfy it.
+                    if (userInput == "4"){
+                        break;
+                    } else if (userInput == "3"){
+                        // FIXME: Need to write the code for each action
+                    }
+                    
+                    
+                    
+                }
+            } else {
+                System.out.println("Something went wrong, no department matched...");
+
+            }
             
             
         } catch (SQLException sqle) {
