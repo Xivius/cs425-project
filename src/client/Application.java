1ebd098f29d53c62287c33cefa627719a1eb7e40
IonatanGiurgiu <32996929+IonatanGiurgiu@users.noreply.github.com>
2020-04-19 17:06:03 -0500

    Login section added, may not work
    Thinking about using "username" as primary key instead of EmployeeID, UserID, etc.

--- src/client/Application.java
+++ src/client/Application.java
@@ -16,26 +16,65 @@ import java.util.Scanner;
 public class Application {
     public static void main(String [] args) {
         Scanner scan = new Scanner(System.in);
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
         String username;
         String password;
         String sql = "";
         
-        /* Login Prompt */
-        System.out.print("Username: ");
-        username = scan.nextLine();
-        System.out.print("Password: ");
-        password = scan.nextLine();
-        
         try {
+            Connection conn = null;
+            Statement stmt = null;
+            ResultSet rs = null;
+            
             conn = DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/postgres", "postgres", "1234"
             );
             stmt = conn.createStatement();
             sql = "SELECT * FROM ";
             rs = stmt.executeQuery(sql);
+            
+            
+            // LOGIN PART
+            // Stay in loop while there are no matches for username and password
+            String privilege = null;
+            while(true){
+                /* Login Prompt */
+                System.out.print("Username: ");
+                username = scan.nextLine();
+                System.out.print("Password: ");
+                password = scan.nextLine();
+                
+                // Find out the priviledge of the person who logged in.
+                // Select privilege... where the username and password matches.
+                /*
+                String getPrivilege = "SELECT Privilege FROM Login JOIN Employee_Login"
+                                        +"ON Login.UserID = Employee_Login.UserID"
+                                        +"WHERE Employee_Login.Username = " + username
+                                        + "AND Employee_Login.Password = " + password;
+                */
+                                        
+                // Or, if the username and password are in the Login table:
+                String getPrivilege = "SELECT Privilege FROM Login WHERE Username ="
+                                        + username + "AND Password = " + password;
+                
+                ResultSet rset = stmt.executeQuery(getPriviledge);                
+                while (rset.next()){
+                    privilege = rset.getString(1);
+                }
+                
+                // If there is no match, keep going in the loop, else break.
+                if (privilege == null){
+                    System.out.println("Username and password does not match");
+                } else {
+                    break;
+                }
+            }
+            
+            
+            
+                                    
+            
+            
+            
         } catch (SQLException sqle) {
             System.out.println(sqle.getMessage());
         }
